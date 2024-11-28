86e2c9dced8bf36b75e13faa8399c85e855ac08a
Rafaelm <isaizanami@gmail.com>
2024-11-27 22:01:52 -0300

    criado arquivo vercel, configurado o vite colocando a base e o tailwind alterado.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+  content: [
+    "./index.html", // Arquivo principal no root
+    "./src/**/*.{html,js,jsx}", // Todos os arquivos HTML, JS e JSX dentro da pasta src
+    "./*.{html,js}", // Qualquer HTML ou JS fora da pasta src
+  ],
   theme: {
-    extend: {},
+    extend: {}, // Para adicionar customizações ao Tailwind futuramente
   },
-  plugins: [],
-}
-
+  safelist: [
+    // Classes dinâmicas que você usa e que podem ser removidas pelo PurgeCSS em produção
+    "border-solid",
+    "border-2",
+    "border-blue-700",
+    "shadow-blue-700",
+    "rounded-xl",
+    "shadow-xl",
+    "h-96",
+    "w-52",
+    "m-2",
+    "flex",
+    "flex-col",
+    "p-2",
+    "justify-between",
+    "group",
+    "feminino",
+    "masculino",
+  ],
+  plugins: [], // Adicione plugins aqui se necessário, como forms ou typography
+};
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/PTictacEnd/",
+  base: "/projetotictac/",
   plugins: [],
   resolve: {
     /*something*/
